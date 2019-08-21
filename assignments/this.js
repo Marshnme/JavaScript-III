2a4917b29bd8ac24a1d4ad38b13b844ac79c7115
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-08-21 15:41:26 -0400

    this challenge complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding - default value of "this" if no other rule is used.
+* 2. Implicit Binding - if the function is invoked using a method then "this" is referencing whats left of the dot.
+* 3. new binding - creates a new function that links the "this" to the new function
+* 4. Explicit binding - call/apply/bind/ to directly choose what "this" refers too. 
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,41 @@
 // Principle 1
 
 // code example for Window Binding
-
+this;
 // Principle 2
 
 // code example for Implicit Binding
 
+let froggy = {
+    name: "toad",
+    age: 1,
+    noise:"RIBBBBBITTT",
+    ribbit: function(){
+    console.log(`toad does a ${this.noise} he is only ${this.age}`);
+}
+}
+froggy.ribbit();
+
 // Principle 3
 
 // code example for New Binding
+function Grapes(param1,param2){
+    this.item1 = param1;
+    this.item2 = param2;
+}
+    myFavGrapes = new Grapes("green","red")
 
+    console.log(`my first favorite is ${myFavGrapes.item2} my second fav is ${myFavGrapes.item1}`)
 // Principle 4
 
 // code example for Explicit Binding
+function treeTalk(){
+    console.log(`this ${this.type} tree is ${this.width} wide. wow!!`)
+}
+const tree = {
+    height:"100ft",
+    width:"600ft",
+    type:"oak"
+}
+
+treeTalk.call(tree);
